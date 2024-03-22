6991ea8855915f2aac1835998571104e254fac77
Ryan Legler <ryan.legler@gmail.com>
2024-03-21 17:02:32 -0700

    test

--- app/api/metadata/route.ts
+++ app/api/metadata/route.ts
@@ -7,25 +7,34 @@ import metaFetcher from "meta-fetcher";
 export const runtime = "edge";
 
 export async function POST(request: NextRequest) {
+  return new Response("Hello", {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+
   // could add other properties here on the body to include notion DB
-  const { url } = await request.json();
+  // const { url } = await request.json();
 
-  // early return if url is not valid
-  if (!isValidUrl(url)) {
-    return new Response("Invalid URL", {
-      status: 400,
-    });
-  }
+  // // early return if url is not valid
+  // if (!isValidUrl(url)) {
+  //   return new Response("Invalid URL", {
+  //     status: 400,
+  //   });
+  // }
 
-  const metaData = await metaFetcher(url);
+  // const metaData = await metaFetcher(url);
 
-  try {
-    return new Response(JSON.stringify(metaData), {
-      status: 200,
-    });
-  } catch (e) {
-    return new Response("Request cannot be processed!", {
-      status: 400,
-    });
-  }
+  // try {
+  //   return new Response(JSON.stringify(metaData), {
+  //     status: 200,
+  //   });
+  // } catch (e) {
+  //   return new Response("Request cannot be processed!", {
+  //     status: 400,
+  //   });
+  // }
 }
