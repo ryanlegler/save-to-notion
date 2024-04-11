0ee9745ccf96fe524667d355f633b4ab02ffb32d
Ryan Legler <ryan.legler@gmail.com>
2024-04-11 08:02:08 -0700

    fix relative links on images

--- app/api/metadata/route.ts
+++ app/api/metadata/route.ts
@@ -3,58 +3,75 @@ import { isValidUrl } from "@/utils/isValidUrl";
 import metaFetcher from "meta-fetcher";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function OPTIONS(req: NextRequest) {
-  return new NextResponse(null, {
-    status: 200,
-    headers: corsHeaders,
-  });
+    return new NextResponse(null, {
+        status: 200,
+        headers: corsHeaders,
+    });
 }
 
+const healRelativeLink = ({ imageUrl, websiteUrl }: { imageUrl: string; websiteUrl: string }) => {
+    if (imageUrl.startsWith("http")) {
+        return imageUrl;
+    }
+    const url = new URL(websiteUrl);
+    return `${url.protocol}//${url.hostname}${imageUrl}`;
+};
+
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const url = searchParams.get("url") || "";
+    const searchParams = request.nextUrl.searchParams;
+    const url = searchParams.get("url") || "";
 
-  if (!isValidUrl(url)) {
-    return new Response("Invalid URL", {
-      status: 400,
-      headers: corsHeaders,
-    });
-  }
+    if (!isValidUrl(url)) {
+        return new Response("Invalid URL", {
+            status: 400,
+            headers: corsHeaders,
+        });
+    }
 
-  const metaDataResponse = await metaFetcher(url);
+    const metaDataResponse = await metaFetcher(url);
 
-  if (!metaDataResponse?.metadata?.banner) {
-    const alternate = await fetch(
-      `http://iframely.server.crestify.com/iframely?url=${url}`
-    );
+    if (!metaDataResponse?.metadata?.banner || !metaDataResponse?.metadata?.title) {
+        const alternate = await fetch(`http://iframely.server.crestify.com/iframely?url=${url}`);
 
-    const data = await alternate.json();
-    const imageLink =
-      data.links.find((value: { type: string }) =>
-        value.type.startsWith("image/")
-      )?.href || "";
+        const data = await alternate.json();
+
+        const imageLink =
+            data.links.find((value: { type: string }) => value.type.startsWith("image/"))?.href ||
+            "";
+
+        const mergedMetaData = {
+            ...metaDataResponse,
+            metadata: {
+                ...metaDataResponse.metadata,
+                banner: healRelativeLink({ imageUrl: imageLink, websiteUrl: url }),
+            },
+        };
+
+        return NextResponse.json(mergedMetaData, {
+            status: 200,
+            headers: corsHeaders,
+        });
+    }
 
     const mergedMetaData = {
-      ...metaDataResponse,
-      metadata: {
-        ...metaDataResponse.metadata,
-        banner: imageLink,
-      },
+        ...metaDataResponse,
+        metadata: {
+            ...metaDataResponse.metadata,
+            banner: healRelativeLink({
+                imageUrl: metaDataResponse.metadata.banner,
+                websiteUrl: url,
+            }),
+        },
     };
 
     return NextResponse.json(mergedMetaData, {
-      status: 200,
-      headers: corsHeaders,
+        status: 200,
+        headers: corsHeaders,
     });
-  }
-
-  return NextResponse.json(metaDataResponse, {
-    status: 200,
-    headers: corsHeaders,
-  });
 }
--- components/Form2.tsx
+++ components/Form2.tsx
@@ -2,34 +2,31 @@
 import { useCallback, useState } from "react";
 
 export function Form2() {
-  const [url, setUrl] = useState("");
+    const [url, setUrl] = useState("");
 
-  const handleChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setUrl(event.target.value);
-    },
-    []
-  );
-  const handleSubmit = useCallback(async () => {
-    const response = await fetch(
-      // `/api/metadata?url=${url}`,
-      `https://save-to-notion-gray.vercel.app/api/metadata?url=${url}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+    const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(event.target.value);
+    }, []);
+    const handleSubmit = useCallback(async () => {
+        const response = await fetch(
+            `/api/metadata?url=${url}`,
+            // `https://save-to-notion-gray.vercel.app/api/metadata?url=${url}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        const result = await response.json();
+        console.log("result", result);
+    }, [url]);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <input value={url} onChange={handleChange} />
+            <button onClick={handleSubmit}>Submit</button>
+        </main>
     );
-
-    const result = await response.json();
-    console.log("result", result);
-  }, [url]);
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <input value={url} onChange={handleChange} />
-      <button onClick={handleSubmit}>Submit</button>
-    </main>
-  );
 }
